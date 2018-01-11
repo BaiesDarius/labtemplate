5ce57da307dd4cb030a6492cd3542bffe94a97c3
DESKTOP-M7QB9IB\dareu <darius_darius91@yahoo.com>
2018-01-11 18:48:22 +0200

    Implemented Register Page

--- frontend/src/app/components/pages/register/models/registerModel.ts
+++ frontend/src/app/components/pages/register/models/registerModel.ts
@@ -2,7 +2,7 @@ export class RegisterUserModel{
 
     public email: string;
     public password: string;
-
+    public confirmPassword: string;
     public firstName: string;
     public lastName: string;
 }
